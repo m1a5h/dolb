b9e073ebecbd129d34d95592b474f8206dc5f2d7
Matthew Ashman <matthewiashman@gmail.com>
2023-01-12 01:23:13 +0000

    Add resource TF file for 2 nginx servers on Digtal Ocean

---
+++ www-1.tf
@@ -0,0 +1,26 @@
+resource "digitalocean_droplet" "www-1" {
+	image = "ubuntu-22-10-x64"
+	name = "www-1"
+	region = "lon1"
+	size = "s-1vcpu-1gb"
+	ssh_keys = [
+		data.digitalocean_ssh_key.terraform.id
+	]
+
+	connection {
+		host = self.ipv4_address
+		user = "root"
+		type = "ssh"
+		private_key = file(var.pvt_key)
+		timeout = "2m"
+	}
+
+	provisioner "remote-exec" {
+		inline = [
+			"export PATH=$PATH:/usr/bin",
+			# install nginx
+			"sudo apt update",
+			"sudo apt install nginx -y"
+		]
+	}
+}
